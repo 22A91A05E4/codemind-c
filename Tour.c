3f1fed90e16c12dd1ae1114e7f08032a75d67000
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2023-01-27 19:16:31 +0530

    #include<stdio.h> int main() {     int t,i;     scanf("%d",&t);     for(i=1;i<=t;i++)     {         int n,m,c;         scanf("%d%d",&n,&m);         c=n*5+m*7;         printf("%d ",c);     } }

---
+++ Tour.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=1;i<=t;i++)
+    {
+        int n,m,c;
+        scanf("%d%d",&n,&m);
+        c=n*5+m*7;
+        printf("%d
+",c);
+    }
+}
